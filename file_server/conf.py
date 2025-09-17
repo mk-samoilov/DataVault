2dd08aa7cd9296187156d3db4e2bcda2cc585b8f
mk-samoilov <me@mk-samoilov.ru>
2025-09-17 22:37:07 +0300

    project structure refactor

--- file_server/conf.py
+++ file_server/conf.py
@@ -4,3 +4,6 @@ from pathlib import Path
 server_core_dir = Path(__file__).resolve().parent
 
 STORAGE_DIR = str(server_core_dir.parent) + "/storage"
+
+MAX_ONE_FILE_SIZE = 32 * 1024 * 1024 * 1024 # 32 GB
+MAX_STORAGE_SIZE = 512 * 1024 * 1024 * 1024 # 512 GB
