9a453709188b88be26bc9aed331f53c59abc6b47
mk <me@mk-samoilov.ru>
2025-09-18 16:53:14 +0300

    default storage size refactor

--- file_server/conf.py
+++ file_server/conf.py
@@ -5,5 +5,5 @@ server_core_dir = Path(__file__).resolve().parent
 
 STORAGE_DIR = str(server_core_dir.parent) + "/storage"
 
-MAX_ONE_FILE_SIZE = 32 * 1024 * 1024 * 1024 # 32 GB
-MAX_STORAGE_SIZE = 512 * 1024 * 1024 * 1024 # 512 GB
+MAX_ONE_FILE_SIZE = 4 * 1024 * 1024 * 1024 # 4 GB
+MAX_STORAGE_SIZE = 16 * 1024 * 1024 * 1024 # 16 GB
